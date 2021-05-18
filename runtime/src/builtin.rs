7ef52cc9615112156a213162565d76d0c2b279aa
Alejandro Soto <alejandro@34project.org>
2021-05-18 09:14:59 -0600

    Implement builtin_{true,false,neg}()

--- runtime/src/builtin.rs
+++ runtime/src/builtin.rs
@@ -1,4 +1,19 @@
 #[no_mangle]
+pub extern "C" fn builtin_true() -> usize {
+    true as usize
+}
+
+#[no_mangle]
+pub extern "C" fn builtin_false() -> usize {
+    false as usize
+}
+
+#[no_mangle]
+pub extern "C" fn builtin_neg(boolean: usize) -> usize {
+    !(boolean & 1 == 1) as usize
+}
+
+#[no_mangle]
 pub extern "C" fn builtin_debug(hint: usize) {
     crate::sys::debug(hint);
 }
