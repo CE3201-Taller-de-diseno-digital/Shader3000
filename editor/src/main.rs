d7d38e1858b2af5835e56b5a097c8ac4ca754588
Jose Alejandro <joalchama@gmail.com>
2021-05-23 17:46:23 -0600

    Add .glade XLM and basic main.rs

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,3 +1,18 @@
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    sourceview::View::static_type();
+
+    let glade_src = include_str!("IDE.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+    let window: gtk::Window = builder.get_object("main_window").unwrap();
+
+    window.show_all();
+
+    gtk::main();
 }
